98f342f2188501de8abbc3256635c5743662943d
FraNzY <anoj1810@gmail.com>
2023-06-10 23:53:46 +0545

    building expense form

---
+++ expense-tracker/src/components/expenseForm.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const expenseForm = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input id="description " type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input id="amount" type="number" className="form-control" />
+      </div>
+    </form>
+  );
+};
+
+export default expenseForm;
