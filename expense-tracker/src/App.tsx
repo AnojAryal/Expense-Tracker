56e8dafaed26747a4cef2437808aa75211b84293
FraNzY <anoj1810@gmail.com>
2023-06-13 21:43:21 +0545

    Building Expense Filter

--- expense-tracker/src/App.tsx
+++ expense-tracker/src/App.tsx
@@ -1,20 +1,31 @@
 import "./App.css";
+import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseList from "./components/ExpenseList";
 import { useState } from "react";
 
 function App() {
-  const [expenses,setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "utilities" },
-    { id: 2, description: "abc", amount: 10, category: "utilities" },
-    { id: 3, description: "def", amount: 10, category: "utilities" },
-    { id: 4, description: "ghi", amount: 10, category: "utilities" },
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "abc", amount: 10, category: "Utilities" },
+    { id: 3, description: "def", amount: 10, category: "Entertainment" },
+    { id: 4, description: "ghi", amount: 10, category: "Groceries" },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCatagory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
-        onDelete={(id) => setExpenses(expenses.filter(e => e.id !==id))}
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
   );
--- expense-tracker/src/components/ExpenseFilter.tsx
+++ expense-tracker/src/components/ExpenseFilter.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { categories } from "../App";
 
 interface Props {
-  onSelectCategory: (category: string) => void;
+  onSelectCatagory: (category: string) => void;
 }
 
-const ExpenseFilter = () => {
+const ExpenseFilter = ({ onSelectCatagory }: Props) => {
   return (
     <select
-      className="form-select"
-      onChange={(event) => onSelectCategory(event.target.value)}
+      className="form-select "
+      onChange={(event) => onSelectCatagory(event.target.value)}
     >
-      <option value=""> All Categories</option>
-      {categories.map((category) => (
-        <option key={category} value={category}>
-          {category}
-        </option>
-      ))}
+      <option value="">All Categories</option>
+      <option value="Groceries">Groceries</option>
+      <option value="Utilities">Utilities</option>
+      <option value="Entertainment">Entertainment</option>
     </select>
   );
 };
